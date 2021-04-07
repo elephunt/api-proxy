d784377076a0cfe6fd9fd2b27013619e7c6f2e61
Roman Margolin <romamargolin@gmail.com>
2021-04-07 16:07:42 +0300

    Add health check

---
+++ src/main/java/com/roman/margolin/api/proxy/proxyapi/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.roman.margolin.api.proxy.proxyapi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+
+    @GetMapping("/health_check")
+    public ResponseEntity health(){
+        return  ResponseEntity.ok().build();
+    }
+
+
+}
