82b8cc2063f255a010539303d1ef4a7a28821177
Roman Margolin <romamargolin@gmail.com>
2021-04-07 17:45:12 +0300

    Add zuul

--- src/main/java/com/roman/margolin/api/proxy/proxyapi/ProxyApiApplication.java
+++ src/main/java/com/roman/margolin/api/proxy/proxyapi/ProxyApiApplication.java
@@ -3,9 +3,11 @@ package com.roman.margolin.api.proxy.proxyapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableZuulProxy
 public class ProxyApiApplication {
 
     public static void main(String[] args) {
