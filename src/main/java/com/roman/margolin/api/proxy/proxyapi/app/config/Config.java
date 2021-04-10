d643c351a160ae80994dd1bffae8c8a4f40a2850
Roman Margolin <romamargolin@gmail.com>
2021-04-10 14:34:40 +0300

    switch to spring gateway

--- src/main/java/com/roman/margolin/api/proxy/proxyapi/app/config/Config.java
+++ src/main/java/com/roman/margolin/api/proxy/proxyapi/app/config/Config.java
@@ -1,5 +1,7 @@
 package com.roman.margolin.api.proxy.proxyapi.app.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.ServerCodecConfigurer;
@@ -7,8 +9,4 @@ import org.springframework.http.codec.ServerCodecConfigurer;
 @Configuration
 public class Config {
 
-    @Bean
-    public ServerCodecConfigurer serverCodecConfigurer() {
-        return ServerCodecConfigurer.create();
-    }
 }
